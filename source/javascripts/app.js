98c82540c8b1fdb9abf310f39015db5801f172de
ilya <ilya@spacebox.ru>
2016-03-25 17:07:16 +0300

    Create index page

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,5 +1,17 @@
 jQuery(function($){
 	
-	
+	/**
+	* Custom select
+	*/
+	(function(){
+		
+		$('.fancybox').fancybox({
+			'width':     '80%',
+			'autoSize':  false,
+			'maxWidth':  980,
+			'padding':   [45, 45, 45, 45]
+		});
+		
+	}());
 	
 });
